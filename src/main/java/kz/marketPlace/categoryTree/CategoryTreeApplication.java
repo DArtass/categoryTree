eaa5f3766da6f6a0603613543fdfdd14cb680a05
Trofimov Alexandr <darax111@mail.ru>
2023-03-18 14:21:14 +0600

    Initial commit

---
+++ src/main/java/kz/marketPlace/categoryTree/CategoryTreeApplication.java
@@ -0,0 +1,13 @@
+package kz.marketPlace.categoryTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/marketPlace/categoryTree/CategoryTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.marketPlace.categoryTree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
